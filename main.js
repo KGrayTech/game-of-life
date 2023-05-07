0506b00c023aaa6f8188f710c833ef6321e30685
KGray <>
2023-05-07 13:29:07 +0300

    feat: Added shaders with uv coord and canvas resolution

--- main.js
+++ main.js
@@ -2,16 +2,38 @@ import "./style.css";
 import { createProgram, createShader, initWebGL } from "./utils";
 
 const vertexShaderSource = `
-  attribute vec2 a_position;
+  precision mediump float;
+
+  attribute vec2 aPosition;
+  attribute vec2 aTexCoord;
+
+  uniform vec2 uResolution;
+
+  varying vec2 vTexCoord;
 
   void main() {
-    gl_Position = vec4(a_position, 0.0, 1.0);
+    vTexCoord = aTexCoord;
+    gl_Position = vec4(aPosition, 0.0, 1.0);
   }
 `;
 
 const fragmentShaderSource = `
+  precision mediump float;
+  varying vec2 vTexCoord;
+
+  uniform vec2 uResolution;
+
   void main() {
-    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    vec2 uv = vTexCoord;
+    uv.x *= uResolution.x / uResolution.y;
+
+    float d = length(uv);
+    float c = d;
+
+    if (d < 0.3) c = 1.0;
+    else c = 0.0;
+
+    gl_FragColor = vec4(vec3(c), 1.0);
   }
 `;
 
@@ -21,6 +43,7 @@ const initApp = () => {
   canvas.height = window.innerHeight;
 
   const glCtx = initWebGL(canvas);
+  glCtx.viewport(0, 0, canvas.width, canvas.height);
 
   if (!glCtx) return;
 
@@ -38,30 +61,43 @@ const initApp = () => {
   const shaderProgram = createProgram(glCtx, vertexShader, fragmentShader);
   glCtx.useProgram(shaderProgram);
 
-  const positionAttributeLocation = glCtx.getAttribLocation(shaderProgram, "a_position");
+  const resolutionUniformLocation = glCtx.getUniformLocation(shaderProgram, "uResolution");
+  glCtx.uniform2f(resolutionUniformLocation, canvas.width, canvas.height);
+ 
+  const texCoordBuffer = glCtx.createBuffer();
+  glCtx.bindBuffer(glCtx.ARRAY_BUFFER, texCoordBuffer);
+  glCtx.bufferData(glCtx.ARRAY_BUFFER, new Float32Array([
+    0.0, 0.0,
+    1.0, 0.0,
+    0.0, 1.0,
+    1.0, 1.0
+  ]), glCtx.STATIC_DRAW);
+  const texCoordLocation = glCtx.getAttribLocation(shaderProgram, "aTexCoord");
+
+
   const positionBuffer = glCtx.createBuffer();
-
   glCtx.bindBuffer(glCtx.ARRAY_BUFFER, positionBuffer);
+  const vertices = [
+    -1.0, -1.0,
+    1.0, -1.0,
+    -1.0, 1.0,
+    1.0, 1.0
+  ]
+  glCtx.bufferData(glCtx.ARRAY_BUFFER, new Float32Array(vertices), glCtx.STATIC_DRAW);
+  const positionAttributeLocation = glCtx.getAttribLocation(shaderProgram, "aPosition");
 
-  const positions = [
-    0.0,
-    0.5,
-    -0.5,
-    -0.5,
-    0.5,
-    -0.5,
-  ];
-
-  glCtx.viewport(0, 0, canvas.width, canvas.height);
-  glCtx.bufferData(glCtx.ARRAY_BUFFER, new Float32Array(positions), glCtx.STATIC_DRAW);
   glCtx.enableVertexAttribArray(positionAttributeLocation);
+  glCtx.enableVertexAttribArray(texCoordLocation);
+
   glCtx.vertexAttribPointer(positionAttributeLocation, 2, glCtx.FLOAT, false, 0, 0);
+  glCtx.vertexAttribPointer(texCoordLocation, 2, glCtx.FLOAT, false, 0, 0);
 
   glCtx.clearColor(0.0, 0.0, 0.0, 1.0);
   glCtx.clear(glCtx.COLOR_BUFFER_BIT);
-  glCtx.drawArrays(glCtx.TRIANGLES, 0, 3);
+  glCtx.drawArrays(glCtx.TRIANGLE_STRIP, 0, 4);
 
   glCtx.disableVertexAttribArray(positionAttributeLocation);
+  glCtx.disableVertexAttribArray(texCoordLocation);
 };
 
 initApp();
